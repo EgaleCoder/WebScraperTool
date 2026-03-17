8f6aa6976037393fc319fa0c14a110ec251358e6
EgaleCoder <ironabhi728@gmail.com>
2026-03-18 03:45:16 +0530

    Project's base folder/files setup.
    - Add project folder structure.
    - Add gitignore file and also add file and folder in this file.
    - Add Scraper Folder and HtmlScraper class in this folder.

---
+++ GSMTool/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using GSMTool.Scraper;
+using System.Windows;
+
+namespace GSMTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HtmlScraper _scraper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _scraper = new HtmlScraper();
+        }
+
+        private async void LoadHtml_Click(object sender, RoutedEventArgs e)
+        {
+
+            /* Load HTML */
+
+            //string url = UrlTextBox.Text;
+            //var html = await _scraper.LoadHtml(url);
+            //OutputTextBox.Text = html;
+
+            /* Load HTML */
+            //string url = UrlTextBox.Text;
+            //var title = await _scraper.GetTitle(url);
+            //OutputTextBox.Text = title;
+
+
+            /* Load Div */
+            string url = UrlTextBox.Text;
+
+            var text = await _scraper.GetDivText(url, "specs-list");
+
+            OutputTextBox.Text = text;
+        }
+    }
+}
