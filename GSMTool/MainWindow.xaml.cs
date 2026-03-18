a492cfce21fbb9d74dd98231a99fec94f44cf7bf
EgaleCoder <ironabhi728@gmail.com>
2026-03-19 05:26:40 +0530

    Adding view specific div with css and js support.
    Add a new NUGET package WebView2 for view specific div view.
    Add a separate window for div view in UI.
    add refresh button in UI with method.

--- GSMTool/MainWindow.xaml.cs
+++ GSMTool/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using GSMTool.Scraper;
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GSMTool
 {
@@ -12,7 +16,23 @@ namespace GSMTool
         public MainWindow()
         {
             InitializeComponent();
+
             _scraper = new HtmlScraper();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await InitBrowser();
+        }
+
+        private async Task InitBrowser()
+        {
+            await BrowserBox.EnsureCoreWebView2Async(null);
+
+            BrowserBox.NavigateToString(
+                "<html><body style='background:white'></body></html>");
         }
 
         private async void LoadHtml_Click(object sender, RoutedEventArgs e)
@@ -22,20 +42,65 @@ namespace GSMTool
 
             //string url = UrlTextBox.Text;
             //var html = await _scraper.LoadHtml(url);
-            //OutputTextBox.Text = html;
+            //OutputWebBox.Text = html;
 
             /* Load HTML */
             //string url = UrlTextBox.Text;
             //var title = await _scraper.GetTitle(url);
-            //OutputTextBox.Text = title;
+            //OutputWebBox.Text = title;
 
 
             /* Load Div */
+
             string url = UrlTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(url))
+                return;
+
+            /* load full page */
+            BrowserBox.CoreWebView2.Navigate(url);
+            await Task.Delay(2500);
 
             var text = await _scraper.GetDivText(url, "specs-list");
 
-            OutputTextBox.Text = text;
+            // show only specs-list div
+            await ShowOnlySpecsDiv();
+
+            OutputDataBox.Text = text;
         }
+
+
+        // Need to fix 19/03/2026: Show full page for a bit then show only specs-list div and can be replace via getdiv method in scrapper?
+        private async Task ShowOnlySpecsDiv()
+        {
+            string script = @"
+                (function(){
+
+                    let div = document.querySelector('#specs-list');
+
+                    if(!div) return;
+
+                    document.body.innerHTML = '';
+
+                    document.body.appendChild(div);
+
+                    document.body.style.background = 'white';
+
+                })();
+            ";
+
+            await BrowserBox.ExecuteScriptAsync(script);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            BrowserBox.NavigateToString(
+                "<html><body style='background:white'></body></html>");
+
+            OutputDataBox.Clear();
+
+            UrlTextBox.Text = "";
+        }
+
     }
 }
--- GSMTool/Scraper/HtmlScraper.cs
+++ GSMTool/Scraper/HtmlScraper.cs
@@ -12,6 +12,12 @@ namespace GSMTool.Scraper
         public HtmlScraper()
         {
             _httpClient = new HttpClient();
+
+            _httpClient.DefaultRequestHeaders.Add(
+               "User-Agent",
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+               "AppleWebKit/537.36 (KHTML, like Gecko) " +
+               "Chrome/141.0.0.0 Safari/537.36");
         }
 
         /* To Load HTML */
@@ -68,6 +74,51 @@ namespace GSMTool.Scraper
                 if (node != null)
                 {
                     return node.InnerText;
+                }
+
+                return "Div not found";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /* To Get Div Web Page */
+        public async Task<string> GetDivHtml(string url, string id)
+        {
+            try
+            {
+
+
+                var html = await _httpClient.GetStringAsync(url);
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                // head
+                var head = doc.DocumentNode.SelectSingleNode("//head");
+
+                // div by id
+                var node = doc.DocumentNode.SelectSingleNode($"//div[@id='{id}']");
+
+                if (node != null)
+                {
+                    string baseTag = $"<base href='{url}' />";
+
+                    string headHtml = head != null
+                    ? head.InnerHtml
+                    : "";
+
+                    string newHtml =
+                   "<html><head>" +
+                    baseTag +
+                    headHtml +
+                    "</head><body>" +
+                     node.OuterHtml +
+                    "</body></html>";
+
+                    return newHtml;
                 }
 
                 return "Div not found";
